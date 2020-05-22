28fb292b1015473b386877eae483fe1032f5e6a6
tong761202 <65745036+tong761202@users.noreply.github.com>
2020-05-22 10:48:50 +0900

    Update box_bps.js

--- box/box_bps.js
+++ box/box_bps.js
@@ -1,6 +1,6 @@
 (function()  {
 	let template = document.createElement("template");
-	template.innerHTML = `
+	template.innerHTML = ;
 		<form id="form">
 			<fieldset>
 				<legend>Color Properties</legend>
@@ -19,7 +19,7 @@
 			padding: 1em 1em 1em 1em;
 		}
 		</style>
-	`;
+	';
 
 	class BoxBps extends HTMLElement {
 		constructor() {
