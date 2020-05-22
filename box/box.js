8602959d0ce937fa8971e8e0d4a3b7aece7f9da7
tong761202 <65745036+tong761202@users.noreply.github.com>
2020-05-22 10:58:13 +0900

    Update box.js

--- box/box.js
+++ box/box.js
@@ -46,8 +46,8 @@
 		      <div class="metric participation" data-ratio=".95">
 		        <svg viewBox="0 0 1000 500">
 			        <path d="M 950 500 A 450 450 0 0 0 50 500"></path>
-					<text class="percentage" text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
-					<text class="title" text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
+					<text class='percentage' text-anchor="middle" alignment-baseline="middle" x="500" y="300" font-size="140" font-weight="bold">0%</text>
+					<text class='title' text-anchor="middle" alignment-baseline="middle" x="500" y="450" font-size="90" font-weight="normal"></text>
   	            </svg>
 		      </div>
 		    </div>
